45f0532aeadb03f41c2454b39396813bd35f59b1
Seraph08 <davidvandenburgh@linux.com>
2016-12-07 20:56:32 -0500

    drop oneplus common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -230,5 +230,3 @@ endif
 # Recovery
 TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.qcom
 
-# inherit from the proprietary version
--include vendor/oneplus/oneplus2/BoardConfigVendor.mk
